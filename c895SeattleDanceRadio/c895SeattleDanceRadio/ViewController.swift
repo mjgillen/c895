362a59dd481873787833974e918726daff0f62c6
mjgillen <mmgillen@me.com>
2018-11-19 14:39:37 -0800

    initial commit

---
+++ c895SeattleDanceRadio/c895SeattleDanceRadio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  c895SeattleDanceRadio
+//
+//  Created by Michael Gillen on 11/19/18.
+//  Copyright © 2018 On The Move Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
